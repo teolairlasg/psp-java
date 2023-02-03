d0e10564d2c269a14ed585c932327046feb7a86b
tlairla <tlairla@B01PROFESOR.IESGUARA.LOCAL>
2023-02-03 08:53:01 +0100

    Clase EstacionMeteo

---
+++ aemet/src/main/java/aemet/EstacionMeteo.java
@@ -0,0 +1,277 @@
+package aemet;
+
+public class EstacionMeteo {
+	private String idema;
+	private float lon;
+	private float lat;
+	private float alt;
+	private String ubi;
+	private String fint;
+	private float prec;
+	private float pacutp;
+	private float pliqtp;
+	private float psolt;
+	private float vmax;
+	private float vv;
+	private float vmaxu;
+	private float vvu;
+	private float dv;
+	private float dvu;
+	private float dmax;
+	private float dmaxu;
+	private float stdvv;
+	private float stdv;
+	private float stdvvu;
+	private float stddvu;
+	private float hr;
+	private float inso;
+	private float pres;
+	private float pres_mar;
+	private float ts;
+	private float tss20cm;
+	private float tss5cm;
+	private float ta;
+	private float tpr;
+	private float tamin;
+	private float tamax;
+	private float vis;
+	private float geo700;
+	private float geo850;
+	private float geo925;
+	private float rviento;
+	private float nieve;
+	public String getIdema() {
+		return idema;
+	}
+	public void setIdema(String idema) {
+		this.idema = idema;
+	}
+	public float getLon() {
+		return lon;
+	}
+	public void setLon(float lon) {
+		this.lon = lon;
+	}
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getAlt() {
+		return alt;
+	}
+	public void setAlt(float alt) {
+		this.alt = alt;
+	}
+	public String getUbi() {
+		return ubi;
+	}
+	public void setUbi(String ubi) {
+		this.ubi = ubi;
+	}
+	public String getFint() {
+		return fint;
+	}
+	public void setFint(String fint) {
+		this.fint = fint;
+	}
+	public float getPrec() {
+		return prec;
+	}
+	public void setPrec(float prec) {
+		this.prec = prec;
+	}
+	public float getPacutp() {
+		return pacutp;
+	}
+	public void setPacutp(float pacutp) {
+		this.pacutp = pacutp;
+	}
+	public float getPliqtp() {
+		return pliqtp;
+	}
+	public void setPliqtp(float pliqtp) {
+		this.pliqtp = pliqtp;
+	}
+	public float getPsolt() {
+		return psolt;
+	}
+	public void setPsolt(float psolt) {
+		this.psolt = psolt;
+	}
+	public float getVmax() {
+		return vmax;
+	}
+	public void setVmax(float vmax) {
+		this.vmax = vmax;
+	}
+	public float getVv() {
+		return vv;
+	}
+	public void setVv(float vv) {
+		this.vv = vv;
+	}
+	public float getVmaxu() {
+		return vmaxu;
+	}
+	public void setVmaxu(float vmaxu) {
+		this.vmaxu = vmaxu;
+	}
+	public float getVvu() {
+		return vvu;
+	}
+	public void setVvu(float vvu) {
+		this.vvu = vvu;
+	}
+	public float getDv() {
+		return dv;
+	}
+	public void setDv(float dv) {
+		this.dv = dv;
+	}
+	public float getDvu() {
+		return dvu;
+	}
+	public void setDvu(float dvu) {
+		this.dvu = dvu;
+	}
+	public float getDmax() {
+		return dmax;
+	}
+	public void setDmax(float dmax) {
+		this.dmax = dmax;
+	}
+	public float getDmaxu() {
+		return dmaxu;
+	}
+	public void setDmaxu(float dmaxu) {
+		this.dmaxu = dmaxu;
+	}
+	public float getStdvv() {
+		return stdvv;
+	}
+	public void setStdvv(float stdvv) {
+		this.stdvv = stdvv;
+	}
+	public float getStdv() {
+		return stdv;
+	}
+	public void setStdv(float stdv) {
+		this.stdv = stdv;
+	}
+	public float getStdvvu() {
+		return stdvvu;
+	}
+	public void setStdvvu(float stdvvu) {
+		this.stdvvu = stdvvu;
+	}
+	public float getStddvu() {
+		return stddvu;
+	}
+	public void setStddvu(float stddvu) {
+		this.stddvu = stddvu;
+	}
+	public float getHr() {
+		return hr;
+	}
+	public void setHr(float hr) {
+		this.hr = hr;
+	}
+	public float getInso() {
+		return inso;
+	}
+	public void setInso(float inso) {
+		this.inso = inso;
+	}
+	public float getPres() {
+		return pres;
+	}
+	public void setPres(float pres) {
+		this.pres = pres;
+	}
+	public float getPres_mar() {
+		return pres_mar;
+	}
+	public void setPres_mar(float pres_mar) {
+		this.pres_mar = pres_mar;
+	}
+	public float getTs() {
+		return ts;
+	}
+	public void setTs(float ts) {
+		this.ts = ts;
+	}
+	public float getTss20cm() {
+		return tss20cm;
+	}
+	public void setTss20cm(float tss20cm) {
+		this.tss20cm = tss20cm;
+	}
+	public float getTss5cm() {
+		return tss5cm;
+	}
+	public void setTss5cm(float tss5cm) {
+		this.tss5cm = tss5cm;
+	}
+	public float getTa() {
+		return ta;
+	}
+	public void setTa(float ta) {
+		this.ta = ta;
+	}
+	public float getTpr() {
+		return tpr;
+	}
+	public void setTpr(float tpr) {
+		this.tpr = tpr;
+	}
+	public float getTamin() {
+		return tamin;
+	}
+	public void setTamin(float tamin) {
+		this.tamin = tamin;
+	}
+	public float getTamax() {
+		return tamax;
+	}
+	public void setTamax(float tamax) {
+		this.tamax = tamax;
+	}
+	public float getVis() {
+		return vis;
+	}
+	public void setVis(float vis) {
+		this.vis = vis;
+	}
+	public float getGeo700() {
+		return geo700;
+	}
+	public void setGeo700(float geo700) {
+		this.geo700 = geo700;
+	}
+	public float getGeo850() {
+		return geo850;
+	}
+	public void setGeo850(float geo850) {
+		this.geo850 = geo850;
+	}
+	public float getGeo925() {
+		return geo925;
+	}
+	public void setGeo925(float geo925) {
+		this.geo925 = geo925;
+	}
+	public float getRviento() {
+		return rviento;
+	}
+	public void setRviento(float rviento) {
+		this.rviento = rviento;
+	}
+	public float getNieve() {
+		return nieve;
+	}
+	public void setNieve(float nieve) {
+		this.nieve = nieve;
+	}
+}
